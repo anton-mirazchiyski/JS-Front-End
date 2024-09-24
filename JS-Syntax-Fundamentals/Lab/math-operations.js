64fd8068df5705946276b1882ce8823a31ffac6b
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-09-24 17:46:26 +0300

    Task "Math-Operations"

---
+++ JS-Syntax-Fundamentals/Lab/math-operations.js
@@ -0,0 +1,29 @@
+function solve(firstNumber, secondNumber, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        case '%':
+            result = firstNumber % secondNumber;
+            break;
+        case '**':
+            result = firstNumber ** secondNumber;
+            break;
+    }
+
+    console.log(result);
+}
+
+solve(5, 6, '+');
+solve(3, 5.5, '*');
