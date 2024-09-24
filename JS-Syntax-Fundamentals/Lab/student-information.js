f7f91c6036ef37bd9217c1314f3d51b8b5da7d92
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-09-24 15:30:21 +0300

    Task "Student-Information"

---
+++ JS-Syntax-Fundamentals/Lab/student-information.js
@@ -0,0 +1,6 @@
+function solve(studentName, studentAge, averageGrade) {
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${averageGrade.toFixed(2)}`);
+}
+
+solve('John', 15, 5.54678);
+solve('Steve', 16, 2.1426);
