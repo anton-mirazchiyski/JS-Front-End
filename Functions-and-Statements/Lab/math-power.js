a3e43a2c27451cf82dd9c9c8edf8cfa9ec964da9
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-10-17 20:00:04 +0300

    Task "Math-Power"

---
+++ Functions-and-Statements/Lab/math-power.js
@@ -0,0 +1,23 @@
+function solve(number, power) {
+    return Math.pow(number, power);
+}
+
+console.log(solve(2, 8));
+
+
+solve2 = (number, power) => number ** power;
+
+console.log(solve2(3, 4));
+
+
+function solve3(number, power) {
+    let result = 1;
+
+    for (let i = 0; i < power; i++) {
+        result *= number
+    }
+
+    return result;
+}
+
+console.log(solve3(2, 8));
