175c3538622f3fba3e5a6da85201c5a0d4e4f99b
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-03-10 16:11:19 +0200

    Task "Person-Info"

---
+++ Objects-and-Classes/Lab/person-info.js
@@ -0,0 +1,19 @@
+function solve(firstName, lastName, age) {
+    const personInfo = {
+        firstName,
+        lastName,
+        age,
+    };
+
+    return personInfo;
+}
+
+console.log(
+    "Peter",
+    "Pan",
+    "20");
+
+console.log(
+    "George",
+    "Smith",
+    "18");
