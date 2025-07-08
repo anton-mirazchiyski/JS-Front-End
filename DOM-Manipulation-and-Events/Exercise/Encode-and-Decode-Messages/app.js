b8565f99404710068c7fc5b09d44a86d80daabbc
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-07-08 22:29:07 +0300

    Task "Encode-and-Decode-Messages"

---
+++ DOM-Manipulation-and-Events/Exercise/Encode-and-Decode-Messages/app.js
@@ -0,0 +1,34 @@
+function encodeAndDecodeMessages() {
+    const firstTextareaElement = document.querySelector('#main > div:first-of-type textarea');
+    const secondTextareaElement = document.querySelector('#main > div:nth-of-type(2) textarea');
+    const encodeButtonElement = document.getElementsByTagName('button')[0];
+    const decodeButtonElement = document.getElementsByTagName('button')[1];
+
+    encodeButtonElement.addEventListener('click', encodeMessage);
+    decodeButtonElement.addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+        const text = firstTextareaElement.value;
+        let encodedMessage = ''
+
+        for (let i = 0; i < text.length; i++) {
+            const newCode = text.charCodeAt(i) + 1;
+            encodedMessage += String.fromCharCode(newCode);
+        }
+
+        secondTextareaElement.value = encodedMessage;
+        firstTextareaElement.value = '';
+    }
+
+    function decodeMessage() {
+        const encodedMessage = secondTextareaElement.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < encodedMessage.length; i++) {
+            const code = encodedMessage.charCodeAt(i) - 1;
+            decodedMessage += String.fromCharCode(code);
+        }
+
+        secondTextareaElement.value = decodedMessage;
+    }
+}
